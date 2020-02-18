28abbb46bf9b333a898c5d76e26b193538a9ef87
bacchusjazz <bacchusjazz6248@gmail.com>
2020-02-19 03:46:22 +0900

    敵モンスターの視界に入ると追跡されるスクリプト実装

--- RPG_Remake/Assets/Scripts/EnemyMove.cs
+++ RPG_Remake/Assets/Scripts/EnemyMove.cs
@@ -5,6 +5,7 @@ using UnityEngine.AI;
 public class EnemyMove : MonoBehaviour
 {
     private NavMeshAgent _agent;
+    private RaycastHit[] _raycastHits = new RaycastHit[10];
 
     private void Start()
     {
@@ -15,7 +16,21 @@ public class EnemyMove : MonoBehaviour
     {
         if (collider.CompareTag("Player"))
         {
-            _agent.destination = collider.transform.position;
+            var positionDiff = collider.transform.position - transform.position; 
+            var distance = positionDiff.magnitude; 
+            var direction = positionDiff.normalized;
+
+            var hitCount = Physics.RaycastNonAlloc(transform.position, direction, _raycastHits, distance);
+            Debug.Log("hitCount: " + hitCount);
+            if (hitCount == 0)
+            {
+                _agent.isStopped = false;
+                _agent.destination = collider.transform.position;
+            }
+            else
+            {
+                _agent.isStopped = true;
+            }
         }
     }
 }
