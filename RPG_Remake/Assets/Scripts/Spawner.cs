2fcc26a231ab851451bc1c7e71aca8abb792afc1
bacchusjazz <bacchusjazz6248@gmail.com>
2020-02-19 07:01:02 +0900

    敵スポーナー実装

---
+++ RPG_Remake/Assets/Scripts/Spawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private PlayerStatus playerStatus;
+    [SerializeField] private GameObject enemyPrefab;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnLoop());
+    }
+
+    private IEnumerator SpawnLoop()
+    {
+        while (true)
+        {
+            var distanceVector = new Vector3(10, 0);
+            var spawnPositionFromPlayer = Quaternion.Euler(0, Random.Range(0, 360f), 0) * distanceVector;
+            var spawnPosition = playerStatus.transform.position + spawnPositionFromPlayer;
+
+            NavMeshHit navMeshHit;
+            if (NavMesh.SamplePosition(spawnPosition, out navMeshHit, 10, NavMesh.AllAreas))
+            {
+                Instantiate(enemyPrefab, navMeshHit.position, Quaternion.identity);
+            }
+
+            yield return new WaitForSeconds(10);
+
+            if (playerStatus.Life <= 0)
+            {
+                break;
+            }
+        }
+    }
+}
