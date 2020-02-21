eb4d6a1eb7d9362f611c5dd6645752e5ff818cb1
bacchusjazz <bacchusjazz6248@gmail.com>
2020-02-21 11:48:55 +0900

    ゲームオーバー処理実装

---
+++ RPG_Remake/Assets/Scripts/GameOverTextAnimator.cs
@@ -0,0 +1,25 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverTextAnimator : MonoBehaviour
+{
+    private void Start()
+    {
+        var transformCache = transform;
+        var defaultPosition = transformCache.localPosition;
+        transformCache.localPosition = new Vector3(0, 300f);
+        transformCache.DOLocalMove(defaultPosition, 1f)
+            .SetEase(Ease.Linear)
+            .OnComplete(() =>
+            {
+                Debug.Log("GAME OVER!!");
+                // シェイク
+                transformCache.DOShakePosition(1.5f, 100);
+            });
+        DOVirtual.DelayedCall(10, () =>
+        {
+            SceneManager.LoadScene("TitleScene");
+        });
+    }
+}
--- RPG_Remake/Assets/Scripts/PlayerStatus.cs
+++ RPG_Remake/Assets/Scripts/PlayerStatus.cs
@@ -1,6 +1,19 @@
 ﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class PlayerStatus : MobStatus
 {
-    // TODO あとでプレイヤー向けにカスタムする
+    protected override void OnDie()
+    {
+        base.OnDie();
+        // ゲームオーバー処理
+        StartCoroutine(GoToGameOverCoroutine());
+    }
+
+    private IEnumerator GoToGameOverCoroutine()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene("GameOverScene");
+    }
 }
